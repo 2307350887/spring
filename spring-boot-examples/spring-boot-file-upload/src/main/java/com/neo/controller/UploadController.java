cc8363987500a6baf84e37a0ea3f40c7fa159c70
2751720193 <2751720193@qq.com>
2019-01-17 23:01:20 +0800

    spring-liuzhihuu

---
+++ spring-boot-examples/spring-boot-file-upload/src/main/java/com/neo/controller/UploadController.java
@@ -0,0 +1,59 @@
+package com.neo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+@Controller
+public class UploadController {
+    //Save the uploaded file to this folder
+    private static String UPLOADED_FOLDER = "E://temp//";
+
+    @GetMapping("/")
+    public String index() {
+        return "upload";
+    }
+
+    @PostMapping("/upload") // //new annotation since 4.3
+    public String singleFileUpload(@RequestParam("file") List<MultipartFile> file,
+                                   RedirectAttributes redirectAttributes) {
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+
+        try {
+            for (MultipartFile fl: file
+                 ) {
+                // Get the file and save it somewhere
+                byte[] bytes = fl.getBytes();
+                Path path = Paths.get(UPLOADED_FOLDER + fl.getOriginalFilename());
+                Files.write(path, bytes);
+
+                redirectAttributes.addFlashAttribute("message",
+                        "You successfully uploaded '" + fl.getOriginalFilename() + "'");
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/uploadStatus";
+    }
+
+    @GetMapping("/uploadStatus")
+    public String uploadStatus() {
+        return "uploadStatus";
+    }
+
+}
